ca56e2cea628f5722fb6f0e993a37a1152bc14a6
George Kaplan <george@georgekaplan.xyz>
2021-12-23 19:58:43 -0500

    add tests to assess correctness, and fix errors found by the tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub trait Node<const BUFFER_SIZE: usize, const SAMPLE_RATE: usize> {
     /// Generate or process serial data.
     ///
     /// The `buffer` parameter serves as both input and output.
-    fn generate(&self, buffer: &mut Buffer<BUFFER_SIZE>);
+    fn generate(&mut self, buffer: &mut Buffer<BUFFER_SIZE>);
 }
 
 /// Internal wrapper for nodes to support graph operations.
@@ -19,7 +19,8 @@ struct NodeContainer<const B: usize, const S: usize> {
     dependents: Vec<usize>,
 }
 
-const EXTERNAL_INPUT: usize = 0;
+/// Use this value in the `Graph::connect` method to indicate that a node receives external input.
+pub const EXTERNAL_INPUT: usize = 0;
 
 /// Orchestrate a group of processing nodes to produce sound.
 ///
@@ -51,25 +52,39 @@ impl<const BUFFER_SIZE: usize, const SAMPLE_RATE: usize> Graph<BUFFER_SIZE, SAMP
 
     /// Removes a node from the signal graph.
     ///
-    /// This also removes all connections to and from this node.
-    pub fn remove_node(&mut self, key: usize) {
-        self.nodes.retain(|n| n.key != key);
+    /// This also removes all connections to and from this node. Returns true if the node was found
+    /// and removed.
+    pub fn remove_node(&mut self, key: usize) -> bool {
+        let mut did_remove = false;
+        self.nodes.retain(|n| {
+            let matching = n.key != key;
+            if !matching {
+                did_remove = true;
+            }
+            matching
+        });
+
         for node in &mut self.nodes {
             node.dependencies.retain(|&v| v != key);
             node.dependents.retain(|&v| v != key);
         }
+
+        did_remove
     }
 
     /// Create a connection between a source node and a sink node.
     ///
     /// When signal is generated, the output of the source will be provided to the sink.
-    pub fn connect(&mut self, source: usize, sink: usize) {
+    pub fn connect(&mut self, source: usize, sink: usize) -> Result<(), ConnectError> {
         // no self connections or cycles allowed
-        if source == sink || self.would_have_cycle(source, sink) {
-            return;
+        if source == sink {
+            return Err(ConnectError::SelfReference);
+        }
+        if self.would_have_cycle(source, sink) {
+            return Err(ConnectError::CyclicalReference);
         }
 
-        if let (Some(source_index), Some(sink_index)) = self.nodes.iter().enumerate().fold(
+        match self.nodes.iter().enumerate().fold(
             (None, None),
             |(maybe_source, maybe_sink), (index, NodeContainer { key, .. })| {
                 if *key == source {
@@ -81,48 +96,80 @@ impl<const BUFFER_SIZE: usize, const SAMPLE_RATE: usize> Graph<BUFFER_SIZE, SAMP
                 }
             },
         ) {
-            self.nodes[source_index].dependents.push(sink);
-            self.nodes[sink_index].dependencies.push(source);
-            if source_index > sink_index {
-                // bubble source node up the list without otherwise disturbing the order
-                for index in (sink_index..source_index).rev() {
-                    self.nodes.swap(index, index + 1);
+            (Some(source_index), Some(sink_index)) => {
+                self.nodes[source_index].dependents.push(sink);
+                self.nodes[sink_index].dependencies.push(source);
+                if source_index > sink_index {
+                    // bubble source node up the list without otherwise disturbing the order
+                    for index in (sink_index..source_index).rev() {
+                        self.nodes.swap(index, index + 1);
+                    }
                 }
             }
+            (None, Some(sink_index)) if source == EXTERNAL_INPUT => {
+                self.nodes[sink_index].dependencies.push(EXTERNAL_INPUT);
+            }
+            _ => (),
         }
+
+        Ok(())
     }
 
     /// Check for a potential cycle in the graph, before creating one.
     fn would_have_cycle(&self, source: usize, sink: usize) -> bool {
+        if source == EXTERNAL_INPUT {
+            return false;
+        }
+
         self.nodes
             .iter()
-            .find(|NodeContainer { key, .. }| *key == sink)
-            .map(|NodeContainer { dependencies, .. }| {
-                dependencies.iter().any(|&d| d == source || self.would_have_cycle(source, d))
-            })
-            .unwrap_or(false)
+            .find(|NodeContainer { key, .. }| *key == source)
+            .expect("Could not find source node when attempting to connect nodes")
+            .dependencies
+            .iter()
+            .any(|&d| d == sink || self.would_have_cycle(d, sink))
     }
 
     /// Remove a connection between nodes.
-    pub fn disconnect(&mut self, source: usize, sink: usize) {
+    ///
+    /// Returns true if a connection was found and removed.
+    pub fn disconnect(&mut self, source: usize, sink: usize) -> bool {
+        let mut did_disconnect = false;
+
         for node in &mut self.nodes {
             if node.key == source {
-                node.dependents.retain(|&v| v != sink);
+                node.dependents.retain(|&v| {
+                    let no_match = v != sink;
+                    if !no_match {
+                        did_disconnect = true;
+                    }
+                    no_match
+                });
             }
             if node.key == sink {
-                node.dependencies.retain(|&v| v != source);
+                node.dependencies.retain(|&v| {
+                    let no_match = v != source;
+
+                    if !no_match {
+                        did_disconnect = true;
+                    }
+
+                    no_match
+                });
             }
         }
+
+        did_disconnect
     }
 
     /// Process the given buffer of input signal.
     ///
     /// The `buffer` parameter serves as both input and output.
-    pub fn generate(&self, buffer: &mut Buffer<BUFFER_SIZE>) {
+    pub fn generate(&mut self, buffer: &mut Buffer<BUFFER_SIZE>) {
         let mut buffer_cache = HashMap::new();
         buffer_cache.insert(EXTERNAL_INPUT, buffer.to_owned());
 
-        for node in &self.nodes {
+        for node in &mut self.nodes {
             let mut buf = [0.0; BUFFER_SIZE];
             sum_buffers(
                 node.dependencies.iter().map(|i| {
@@ -136,23 +183,26 @@ impl<const BUFFER_SIZE: usize, const SAMPLE_RATE: usize> Graph<BUFFER_SIZE, SAMP
             buffer_cache.insert(node.key, buf);
         }
 
-        let terminal_nodes: Vec<_> = self
-            .nodes
-            .iter()
-            .filter(|NodeContainer { dependents, .. }| dependents.is_empty())
-            .map(|NodeContainer { key, .. }| *key)
-            .collect();
-
         *buffer = [0.0; BUFFER_SIZE];
         sum_buffers(
-            terminal_nodes.iter().map(|i| {
-                buffer_cache
-                    .get(i)
-                    .expect("Cache miss - this indicates a logic error in agora::Graph")
-            }),
+            self.nodes
+                .iter()
+                // find terminal nodes
+                .filter(|NodeContainer { dependents, .. }| dependents.is_empty())
+                .map(|NodeContainer { key, .. }| {
+                    buffer_cache
+                        .get(key)
+                        .expect("Cache miss - this indicates a logic error in agora::Graph")
+                }),
             buffer,
         );
     }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ConnectError {
+    SelfReference,
+    CyclicalReference,
 }
 
 /// Sum all inputs into a buffer.
@@ -173,5 +223,148 @@ where
         buffer
             .iter_mut()
             .for_each(|sample| *sample /= num_inputs as f32);
+    }
+}
+
+#[cfg(test)]
+mod node_management {
+    use super::{Buffer, ConnectError, Graph, Node};
+
+    struct NopNode;
+    impl<const B: usize, const S: usize> Node<B, S> for NopNode {
+        fn generate(&mut self, _buffer: &mut Buffer<B>) {}
+    }
+
+    #[test]
+    fn can_insert_noop_node() {
+        let mut g = Graph::<1, 1>::default();
+        g.add_node(Box::new(NopNode));
+    }
+
+    #[test]
+    fn can_remove_node() {
+        let mut g = Graph::<1, 1>::default();
+        let node_a = g.add_node(Box::new(NopNode));
+        assert!(g.remove_node(node_a));
+    }
+
+    #[test]
+    fn reports_removal_status() {
+        let mut g = Graph::<1, 1>::default();
+        assert!(!g.remove_node(4));
+    }
+
+    #[test]
+    fn can_connect_nodes() -> Result<(), ConnectError> {
+        let mut g = Graph::<1, 1>::default();
+        let node_a = g.add_node(Box::new(NopNode));
+        let node_b = g.add_node(Box::new(NopNode));
+        g.connect(node_a, node_b)
+    }
+
+    #[test]
+    fn can_detect_self_references() {
+        let mut g = Graph::<1, 1>::default();
+        let node_a = g.add_node(Box::new(NopNode));
+        let connect_result = g.connect(node_a, node_a);
+        assert!(connect_result.is_err());
+        assert_eq!(connect_result.unwrap_err(), ConnectError::SelfReference);
+    }
+
+    #[test]
+    fn can_detect_cyclical_references() {
+        let mut g = Graph::<1, 1>::default();
+        let node_a = g.add_node(Box::new(NopNode));
+        let node_b = g.add_node(Box::new(NopNode));
+        let node_c = g.add_node(Box::new(NopNode));
+        assert!(g.connect(node_a, node_b).is_ok());
+        assert!(g.connect(node_b, node_c).is_ok());
+        let connect_result = g.connect(node_c, node_a);
+        assert!(connect_result.is_err());
+        assert_eq!(connect_result.unwrap_err(), ConnectError::CyclicalReference);
+    }
+
+    #[test]
+    fn can_disconnect_nodes() {
+        let mut g = Graph::<1, 1>::default();
+        let node_a = g.add_node(Box::new(NopNode));
+        let node_b = g.add_node(Box::new(NopNode));
+        assert!(g.connect(node_a, node_b).is_ok());
+        assert!(g.disconnect(node_a, node_b));
+    }
+
+    #[test]
+    fn reports_disconnect_status() {
+        let mut g = Graph::<1, 1>::default();
+        let node_a = g.add_node(Box::new(NopNode));
+        let node_b = g.add_node(Box::new(NopNode));
+        assert!(!g.disconnect(node_a, node_b));
+    }
+}
+
+#[cfg(test)]
+mod signal_processing {
+    use super::{Buffer, Graph, Node, EXTERNAL_INPUT};
+    use std::f32::consts::{PI, TAU};
+
+    struct DcOffsetNode(f32);
+    impl<const B: usize, const S: usize> Node<B, S> for DcOffsetNode {
+        fn generate(&mut self, buffer: &mut Buffer<B>) {
+            buffer.iter_mut().for_each(|v| *v += self.0);
+        }
+    }
+
+    #[test]
+    fn generates_basic_signal() {
+        let mut g = Graph::<20, 1>::default();
+        g.add_node(Box::new(DcOffsetNode(1.0)));
+        let mut buffer = [0.0; 20];
+        g.generate(&mut buffer);
+        assert_eq!(buffer, [1.0; 20]);
+    }
+
+    #[test]
+    fn processes_external_signal() {
+        let mut g = Graph::<20, 1>::default();
+        let node_a = g.add_node(Box::new(DcOffsetNode(-2.0)));
+        g.connect(EXTERNAL_INPUT, node_a).unwrap();
+        let mut buffer = [1.0; 20];
+        g.generate(&mut buffer);
+        assert_eq!(buffer, [-1.0; 20]);
+    }
+
+    struct PulseNode {
+        frequency: f32,
+        current_phase: f32,
+    }
+    impl<const B: usize, const S: usize> Node<B, S> for PulseNode {
+        fn generate(&mut self, buffer: &mut Buffer<B>) {
+            buffer.fill_with(|| {
+                let last_phase = self.current_phase;
+                self.current_phase += TAU / (self.frequency * S as f32);
+                self.current_phase %= TAU;
+                if last_phase < PI {
+                    1.0
+                } else {
+                    0.0
+                }
+            });
+        }
+    }
+
+    #[test]
+    fn generates_wave_statefully() {
+        let mut g = Graph::<20, 2>::default();
+        g.add_node(Box::new(PulseNode {
+            frequency: 1.0,
+            current_phase: 0.0,
+        }));
+        let mut buffer = [0.0; 20];
+        g.generate(&mut buffer);
+        let expected = [
+            1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0,
+            0.0, 1.0, 0.0,
+        ];
+        assert_eq!(buffer, expected);
     }
 }
